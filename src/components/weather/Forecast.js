c24d961a91203ef1b3933f8ace54a77a3d1b1e5d
mserjanaj-klick <mserjanaj@klick.com>
2022-02-10 11:02:28 +0100

    fixes

--- src/components/weather/Forecast.js
+++ src/components/weather/Forecast.js
@@ -1,51 +1,79 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import moment from "moment";
 import "../../assets/styles/customstyles/forecast.scss";
 import Accordion from "../Accordion";
+/* import trees from "../../../public/images/trees.jpg";
+import clear from "../../../public/images/clear.jpg";
+import cloudy from "../../../public/images/cloud.jpg";
+import lightning from "../../../public/images/lightning.jpg";
+import rainy from "../../../public/images/rainy.jpg";
+import snowy from "../../../public/images/snowy.jpg";
+import sunny from "../../../public/images/sunny.jpg"; */
 
 export default function Forecast() {
+  /* const bgImage = [trees, clear, cloudy, lightning, rainy, snowy, sunny]; */
+
+  const d = new Date();
+
+  const date = d.getDate();
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = weekday[d.getDay()];
+  const mon = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = mon[d.getMonth()];
+  const year = d.getFullYear();
+
   const [city, setCity] = useState(null);
   const [search, setSearch] = useState(null);
 
-  /* const getWeather = () => {
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=a2bd3f9f0b9eea7c0aa1803544a2168a&units=metric`
-      )
-      .then((res) => {
-        const response = res.data;
-        setCity(response);
-      });
-  };
-
   useEffect(() => {
-    getWeather();
-  }, [search]);  */
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=a2bd3f9f0b9eea7c0aa1803544a2168a&units=metric`
-      )
-      .then((res) => {
-        setCity(res.data);
-      });
-  });
+    if (search) {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=a2bd3f9f0b9eea7c0aa1803544a2168a&units=metric`
+        )
+        .then((res) => {
+          setCity(res.data);
+        })
+        .catch(() => {
+          setCity(null);
+        });
+    } else {
+      setCity(null);
+    }
+  }, [search]);
 
   return (
     <div className="forecast">
       <div
         id="forecast-card"
-        className="animate__animated animate__fadeInDown mx-auto my-8 w-[90%] md:w-[40%] lg:w-3/12 h-[40rem] lg:h-[45rem] rounded-3xl shadow-2xl"
+        className="animate__animated animate__fadeInDown mx-auto my-8 w-[90%] md:w-[40%] lg:w-3/12 h-[40rem] lg:h-[50rem] rounded-3xl shadow-2xl"
       >
         <div className="flex justify-center">
           <input
             className="mx-auto mt-8 md:mt-16 mb-2 md:mb-4 p-[0.8rem] w-48 md:w-64 text-lg text-white capitalize bg-transparent rounded-xl"
             type="search"
-            onChange={(event) => {
-              setSearch(event.target.value);
-            }}
+            onChange={(event) => setSearch(event.target.value)}
           />
         </div>
         {!city ? (
@@ -57,8 +85,8 @@ export default function Forecast() {
         ) : (
           <div className="mx-auto flex justify-center">
             <div id="info" className="text-white">
-              <p className="text-center text-sm italic animate__animated animate__fadeIn">
-                {moment().format("dddd")},&nbsp;{moment().format("LL")}
+              <p className="date text-center text-sm italic animate__animated animate__fadeIn">
+                {day}.&nbsp;{date} {month} {year}
               </p>
               <h1 className="my-4 md:my-8 text-2xl md:text-4xl animate__animated animate__fadeIn">
                 <span className="capitalize">{search}</span>&nbsp;
@@ -67,7 +95,7 @@ export default function Forecast() {
                 </span>
               </h1>
               <img
-                src={city.weather[0].icon}
+                src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
                 alt="weather icon"
                 className="animate__animated animate__fadeIn"
               />
@@ -77,7 +105,7 @@ export default function Forecast() {
               <p className="text-center text-md md:text-lg animate__animated animate__zoomIn">
                 {city.weather[0].description}
               </p>
-              <div className="flex justify-center">
+              <div className="flex justify-center mx-auto my-4">
                 <Accordion
                   title="More"
                   feelsLike={city.main.feels_like}
@@ -89,7 +117,7 @@ export default function Forecast() {
               </div>
             </div>
           </div>
-        )}{" "}
+        )}
         <div className="flex justify-center">
           <p className="absolute bottom-8 text-white text-xs">
             &copy;Megi Serjanaj ~ February 2022.
